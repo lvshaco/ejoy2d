f88aba8f64da5e1b42eb92d59c2a04280d384578
lvshaco <lvshaco@lvshacodeMacBook-Pro.local>
2015-08-20 16:08:37 +0800

    ui image

---
+++ examples/asset/image_uc.lua
@@ -0,0 +1,103 @@
+return {
+export="image",
+node={
+  uitype="composite",
+  export="node", --41,
+  xlayout="l",
+  ylayout="t",
+  w=0,
+  h=0,
+  x=-234,
+  y=135,
+  xscale=1.000000,
+  yscale=1.000000,
+  {
+    uitype="button",
+    export="node_Button_1", --30,
+    xlayout="l",
+    ylayout="t",
+    w=46,
+    h=36,
+    x=-257,
+    y=-153,
+    xscale=1.000000,
+    yscale=1.000000,
+    param="Button_1"
+  },
+  {
+    uitype="checkbox",
+    export="node_CheckBox_1", --31,
+    xlayout="l",
+    ylayout="t",
+    w=40,
+    h=40,
+    x=-166,
+    y=-157,
+    xscale=1.000000,
+    yscale=1.000000
+  },
+  {
+    uitype="button",
+    export="node_Button_2", --33,
+    xlayout="l",
+    ylayout="t",
+    w=46,
+    h=36,
+    x=-255,
+    y=-78,
+    xscale=1.000000,
+    yscale=1.000000,
+    param="Button"
+  },
+  {
+    uitype="label",
+    export="node_Text_1", --34,
+    xlayout="l",
+    ylayout="t",
+    w=91,
+    h=23,
+    x=-176,
+    y=-69,
+    xscale=1.000000,
+    yscale=1.000000,
+    param="Text Label"
+  },
+  {
+    uitype="sliderbar",
+    export="node_Slider_1", --36,
+    xlayout="l",
+    ylayout="t",
+    w=200,
+    h=14,
+    x=-7,
+    y=-146,
+    xscale=1.000000,
+    yscale=1.000000
+  },
+  {
+    uitype="processbar",
+    export="node_LoadingBar_1", --38,
+    xlayout="l",
+    ylayout="t",
+    w=200,
+    h=14,
+    x=-23,
+    y=-75,
+    xscale=1.000000,
+    yscale=1.000000
+  },
+  {
+    uitype="listview",
+    export="node_ListView_1", --40,
+    xlayout="l",
+    ylayout="t",
+    w=200,
+    h=200,
+    x=-256,
+    y=-6,
+    xscale=1.000000,
+    yscale=1.000000,
+    param=10
+  }
+}
+}
