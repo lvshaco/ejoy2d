1c374ed1046bd61db7dab6530460901cd6c0b86e
lvshaco <lvshaco@lvshacodeMacBook-Pro.local>
2015-08-20 16:07:55 +0800

    ui image

--- examples/asset/image.lua
+++ examples/asset/image.lua
@@ -2,12 +2,309 @@ return {
 {
     type = "picture",
     id = 0,
-    export="btn_highlight.png",
-    { tex = 1, src = {4,4,68,4,68,36,4,36}, screen = {0,0,1024,0,1024,512,0,512} },
+    export="Button_Disable.png",
+    { tex = 1, src = {610,342,656,342,656,378,610,378}, screen = {0,0,736,0,736,576,0,576} },
 },
 {
     type = "picture",
     id = 1,
-    export="btn_normal.png",
-    { tex = 1, src = {74,4,138,4,138,36,74,36}, screen = {0,0,1024,0,1024,512,0,512} },
+    export="LoadingBarFile.png",
+    { tex = 1, src = {822,210,1022,210,1022,224,822,224}, screen = {0,0,3200,0,3200,224,0,224} },
+},
+{
+    type = "picture",
+    id = 2,
+    export="SliderNode_Normal.png",
+    { tex = 1, src = {586,316,606,316,606,336,586,336}, screen = {0,0,320,0,320,320,0,320} },
+},
+{
+    type = "picture",
+    id = 3,
+    export="Sprite3D.png",
+    { tex = 1, src = {638,264,684,264,684,310,638,310}, screen = {0,0,736,0,736,736,0,736} },
+},
+{
+    type = "picture",
+    id = 4,
+    export="TextAtlas.png",
+    { tex = 1, src = {612,316,776,316,776,334,612,334}, screen = {64,0,2688,0,2688,288,64,288} },
+},
+{
+    type = "picture",
+    id = 5,
+    export="Button_Press.png",
+    { tex = 1, src = {732,264,774,264,774,296,732,296}, screen = {32,32,704,32,704,544,32,544} },
+},
+{
+    type = "picture",
+    id = 6,
+    export="arrowX.png",
+    { tex = 1, src = {732,302,738,302,738,310,732,310}, screen = {48,32,144,32,144,160,48,160} },
+},
+{
+    type = "picture",
+    id = 7,
+    export="arrowZ.png",
+    { tex = 1, src = {744,302,750,302,750,310,744,310}, screen = {48,32,144,32,144,160,48,160} },
+},
+{
+    type = "picture",
+    id = 8,
+    export="arrowY.png",
+    { tex = 1, src = {780,288,786,288,786,298,780,298}, screen = {48,16,144,16,144,176,48,176} },
+},
+{
+    type = "picture",
+    id = 9,
+    export="ImageFile.png",
+    { tex = 1, src = {586,264,632,264,632,310,586,310}, screen = {0,0,736,0,736,736,0,736} },
+},
+{
+    type = "picture",
+    id = 10,
+    export="SliderNode_Disable.png",
+    { tex = 1, src = {584,350,604,350,604,370,584,370}, screen = {0,0,320,0,320,320,0,320} },
+},
+{
+    type = "picture",
+    id = 11,
+    export="missing.png",
+    { tex = 1, src = {516,280,580,280,580,344,516,344}, screen = {0,0,1024,0,1024,1024,0,1024} },
+},
+{
+    type = "picture",
+    id = 12,
+    export="Slider_PressBar.png",
+    { tex = 1, src = {822,250,1016,250,1016,258,822,258}, screen = {48,48,3152,48,3152,176,48,176} },
+},
+{
+    type = "picture",
+    id = 13,
+    export="DemoPlayer0.png",
+    { tex = 1, src = {586,210,816,210,816,258,586,258}, screen = {0,0,3680,0,3680,768,0,768} },
+},
+{
+    type = "picture",
+    id = 14,
+    export="alpha.png",
+    { tex = 1, src = {516,376,517,376,517,377,516,377}, screen = {0,0,16,0,16,16,0,16} },
+},
+{
+    type = "picture",
+    id = 15,
+    export="CheckBox_Press.png",
+    { tex = 1, src = {690,264,726,264,726,300,690,300}, screen = {32,32,608,32,608,608,32,608} },
+},
+{
+    type = "picture",
+    id = 16,
+    export="Button_Normal.png",
+    { tex = 1, src = {754,340,800,340,800,376,754,376}, screen = {0,0,736,0,736,576,0,576} },
+},
+{
+    type = "picture",
+    id = 17,
+    export="missing-font.png",
+    { tex = 1, src = {4,4,510,4,510,380,4,380}, screen = {32,16,8128,16,8128,6032,32,6032} },
+},
+{
+    type = "picture",
+    id = 18,
+    export="CheckBox_Disable.png",
+    { tex = 1, src = {662,340,702,340,702,380,662,380}, screen = {0,0,640,0,640,640,0,640} },
+},
+{
+    type = "picture",
+    id = 19,
+    export="defaultBMFont.png",
+    { tex = 1, src = {516,4,1022,4,1022,204,516,204}, screen = {32,16,8128,16,8128,3216,32,3216} },
+},
+{
+    type = "picture",
+    id = 20,
+    export="Slice3D.png",
+    { tex = 1, src = {638,264,684,264,684,310,638,310}, screen = {0,0,736,0,736,736,0,736} },
+},
+{
+    type = "picture",
+    id = 21,
+    export="SliderNode_Press.png",
+    { tex = 1, src = {780,264,798,264,798,282,780,282}, screen = {16,16,304,16,304,304,16,304} },
+},
+{
+    type = "picture",
+    id = 22,
+    export="CheckBox_Normal.png",
+    { tex = 1, src = {708,340,748,340,748,380,708,380}, screen = {0,0,640,0,640,640,0,640} },
+},
+{
+    type = "picture",
+    id = 23,
+    export="CheckBoxNode_Disable.png",
+    { tex = 1, src = {516,350,544,350,544,370,516,370}, screen = {80,160,528,160,528,480,80,480} },
+},
+{
+    type = "picture",
+    id = 24,
+    export="Slider_Back.png",
+    { tex = 1, src = {822,230,1022,230,1022,244,822,244}, screen = {0,0,3200,0,3200,224,0,224} },
+},
+{
+    type = "picture",
+    id = 25,
+    export="CheckBoxNode_Normal.png",
+    { tex = 1, src = {550,350,578,350,578,370,550,370}, screen = {80,160,528,160,528,480,80,480} },
+},
+{
+    type = "picture",
+    id = 26,
+    export="Sprite.png",
+    { tex = 1, src = {638,264,684,264,684,310,638,310}, screen = {0,0,736,0,736,736,0,736} },
+},
+{
+    type = "picture",
+    id = 27,
+    export="white.png",
+    { tex = 1, src = {523,376,524,376,524,377,523,377}, screen = {0,0,16,0,16,16,0,16} },
+},
+{
+    type = "picture",
+    id = 28,
+    export="defaultMap.png",
+    { tex = 1, src = {516,210,580,210,580,274,516,274}, screen = {0,0,1024,0,1024,1024,0,1024} },
+},
+{
+    type = "label",
+    id = 29,
+    
+    color=0xff414146, align=2, size=14, width=46.0, height=14, noedge=true, space_w=0, space_h=0, auto_size=0
+},
+{
+    type = "animation", 
+    id = 30,
+    export="node_Button_1",
+    component = {
+        {id=16},
+        {id=5},  {id=29,name="label"},
+    },
+    {
+        {0,{index=2,mat={1024,0,0,1024,0,176}}},
+        {1,{index=2,mat={1024,0,0,1024,0,176}}}
+    },
+},
+{
+    type = "animation",
+    id = 31,
+    export="node_CheckBox_1",
+    component = {
+        {id=22},
+        {id=15}, 
+    },
+    {
+        {0},
+        {1}
+    },
+},
+{
+    type = "label",
+    id = 32,
+    
+    color=0xff414146, align=2, size=14, width=46.0, height=14, noedge=true, space_w=0, space_h=0, auto_size=0
+},
+{
+    type = "animation", 
+    id = 33,
+    export="node_Button_2",
+    component = {
+        {id=16},
+        {id=5},  {id=32,name="label"},
+    },
+    {
+        {0,{index=2,mat={1024,0,0,1024,0,176}}},
+        {1,{index=2,mat={1024,0,0,1024,0,176}}}
+    },
+},
+{
+    type = "label",
+    id = 34,
+    export="node_Text_1",
+    color=0xffffffff, align=0, size=20, width=91.0, height=23.0, noedge=true, space_w=0, space_h=0, auto_size=0
+},
+{
+    type = "animation", 
+    id = 35,
+    
+    component = {
+        {id=2},
+        {id=21},  
+    },
+    {
+        {0},
+        {1}
+    },
+},
+{
+    type = "animation",
+    id = 36,
+    export="node_Slider_1",
+    component = {
+        {id=35, name="degree"},
+        {id=24}, 
+    },
+    {
+        {{index=1},{index=0,mat={1024,0,0,1024,0,-48}, touch=true}},
+    },
+},
+{
+    type = "pannel", 
+    id = 37,
+    width=200.0, height=14.0, scissor=true,
+},
+{
+    type = "animation",
+    id = 38,
+    export="node_LoadingBar_1",
+    component = {
+        {id=1, name="bar"},
+        {id=37}, 
+    },
+    {
+        {1,0},
+    },
+},
+{
+    type = "pannel", 
+    id = 39,
+    width=200.0, height=200.0, scissor=true,
+},
+{
+    type = "animation",
+    id = 40,
+    export="node_ListView_1",
+    component = {
+        {id=39,name="pannel"},
+        {name="item1"},
+        {name="item2"},
+        {name="item3"},
+        {name="item4"},
+        {name="item5"},
+        {name="item6"},
+        {name="item7"},
+        {name="item8"},
+        {name="item9"},
+        {name="item10"}
+    },
+    {
+        {{index=0,touch=true},
+        {index=1,touch=true},
+        {index=2,touch=true},
+        {index=3,touch=true},
+        {index=4,touch=true},
+        {index=5,touch=true},
+        {index=6,touch=true},
+        {index=7,touch=true},
+        {index=8,touch=true},
+        {index=9,touch=true},
+        {index=10,touch=true}}
+    },
 },}
