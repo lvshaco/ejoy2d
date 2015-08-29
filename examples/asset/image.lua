c8c0deba203c477de81365ddb226ca5f13a10824
lvshaco <lvshaco@lvshacodeMacBook-Pro.local>
2015-08-29 14:03:20 +0800

    image

--- examples/asset/image.lua
+++ examples/asset/image.lua
@@ -1,25 +1,25 @@
 return {
-{ type = "animation",
+{
+    type = "picture",
     id = 0,
-    component = {
-        {id=1},
-        {id=2},
-    },
-    {
-        {0,{index=1,mat={1024,0,0,1024,640,640}}},
-    }
+    export="pet.png",
+    { tex = 1, src = {4,4,576,4,576,638,4,638}, screen = {0,0,9152,0,9152,10144,0,10144} },
 },
-
 {
     type = "picture",
     id = 1,
     export="btn_highlight.png",
-    { tex = 1, src = {4,4,68,4,68,36,4,36}, screen = {0,0,1024,0,1024,512,0,512} },
+    { tex = 1, src = {582,410,646,410,646,442,582,442}, screen = {0,0,1024,0,1024,512,0,512} },
 },
 {
     type = "picture",
     id = 2,
-    export="btn_normal.png",
-    { tex = 1, src = {74,4,138,4,138,36,74,36}, screen = {0,0,1024,0,1024,512,0,512} },
+    export="back.png",
+    { tex = 1, src = {582,4,782,4,782,404,582,404}, screen = {0,0,3200,0,3200,6400,0,6400} },
 },
-}
+{
+    type = "picture",
+    id = 3,
+    export="btn_normal.png",
+    { tex = 1, src = {652,410,716,410,716,442,652,442}, screen = {0,0,1024,0,1024,512,0,512} },
+},}
